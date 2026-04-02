cddd7a216c1d74bc6f91290e34293f220938a551
KaviPriya0707 <kavipriyam0707@gmail.com>
2026-04-02 20:02:00 +0530

    Add files via upload

---
+++ UC8.java
@@ -0,0 +1,66 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class UC8{
+
+    public static Map<Character, String[]> buildCharacterPatterns() {
+
+        Map<Character, String[]> patternMap = new HashMap<>();
+
+        patternMap.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        patternMap.put('P', new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        patternMap.put('S', new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        });
+
+        return patternMap;
+    }
+
+    public static void renderBanner(String message, Map<Character, String[]> patternMap) {
+
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : message.toCharArray()) {
+                String[] pattern = patternMap.get(ch);
+                line.append(pattern[row]).append(" ");
+            }
+
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Map<Character, String[]> patternMap = buildCharacterPatterns();
+
+        String message = "OOPS";
+
+        renderBanner(message, patternMap);
+    }
+}
